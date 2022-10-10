28b9ce768021232e3d583ea91a1c74505e583699
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-10-10 11:14:44 +0800

    feat(pages): 新增关于页面

--- main.go
+++ main.go
@@ -6,9 +6,21 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprint(w, "<h1>Hello，这里是 goblog</h1>")
-	if err != nil {
-		return
+	if r.URL.Path == "/" {
+		_, err := fmt.Fprint(w, "<h1>Hello，这里是 goblog</h1>")
+		if err != nil {
+			return
+		}
+	} else if r.URL.Path == "/about" {
+		_, err := fmt.Fprint(w, "此博客是用以记录变成笔记，如您有反馈或建议，请联系 "+"<a href=\"mailto:zhaiyuxin103@hotmail.com\">zhaiyuxin103@hotmail.com</a>")
+		if err != nil {
+			return
+		}
+	} else {
+		_, err := fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+"<p>如有疑惑，请联系我们。</p>")
+		if err != nil {
+			return
+		}
 	}
 }
 
