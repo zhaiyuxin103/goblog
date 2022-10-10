b2d01f4fd4355ba484fc4f4846c81b218565552f
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-10-10 12:43:34 +0800

    perf(pages): 404 状态码

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		_, err := fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+"<p>如有疑惑，请联系我们。</p>")
 		if err != nil {
 			return
