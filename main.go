cef25e88fb7eb1275c1e0806a94c2a4c2c4fbda1
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-11-22 10:57:24 +0800

    feat(articles): 新建博文表单

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var router = mux.NewRouter()
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprint(w, "<h1>Hello，欢迎来到 goblog！</h1>")
 	if err != nil {
@@ -29,15 +31,6 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	_, err := fmt.Fprint(w, "文章 ID："+id)
-	if err != nil {
-		return
-	}
-}
-
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprint(w, "访问文章列表")
 	if err != nil {
@@ -45,8 +38,41 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	html := `
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>创建文章 -- 我的技术博客</title>
+</head>
+<body>
+	<form action="%s" method="post">
+		<p><input type="text" name="title"></p>
+		<p><textarea name="body" cols="30" rows="10"></textarea></p>
+		<p><button type="submit">提交</button></p>
+	</form>
+</body>
+</html>
+`
+	storeURL, _ := router.Get("articles.store").URL()
+	fmt.Fprintf(w, html, storeURL)
+}
+
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprint(w, "创建新的文章")
+	if err != nil {
+		return
+	}
+}
+
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	_, err := fmt.Fprint(w, "文章 ID："+id)
 	if err != nil {
 		return
 	}
@@ -74,14 +100,13 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 }
 
 func main() {
-	router := mux.NewRouter()
-
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
-	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
 
 	// 自定义 404 页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
