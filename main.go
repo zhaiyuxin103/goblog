f88a1d89646b9b76f36e159f34716bd07b11bfde
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-11-23 23:27:21 +0800

    feat(articles): 显示文章

--- main.go
+++ main.go
@@ -25,6 +25,12 @@ type ArticlesFormData struct {
 	Errors      map[string]string
 }
 
+// Article 对应一条文章数据
+type Article struct {
+	Title, Body string
+	ID          int64
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprint(w, "<h1>Hello，欢迎来到 goblog！</h1>")
 	checkError(err)
@@ -112,10 +118,38 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+
+	// 1. 获取 URL 参数
 	vars := mux.Vars(r)
 	id := vars["id"]
-	_, err := fmt.Fprint(w, "文章 ID："+id)
-	checkError(err)
+
+	// 2. 读取对应的文章数据
+	article := Article{}
+	query := "SELECT * FROM articles WHERE id = ?"
+	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
+
+	// 3. 如果出现错误
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// 3.1 数据未找到
+			w.WriteHeader(http.StatusNotFound)
+			_, err := fmt.Fprint(w, "404 文章未找到")
+			checkError(err)
+		} else {
+			// 3.2 数据库错误
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			_, err := fmt.Fprint(w, "500 服务器内部错误")
+			checkError(err)
+		}
+	} else {
+		// 4. 读取成功
+		tmpl, err := template.ParseFiles("resources/views/articles/show.tmpl")
+		checkError(err)
+
+		err = tmpl.Execute(w, article)
+		checkError(err)
+	}
 }
 
 func saveArticleToDB(title string, body string) (int64, error) {
