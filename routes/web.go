ab72335fef6a9d97995c3b062a061414cc3b92ed
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-12-07 12:04:52 +0800

    build(deps): add tailwindcss

--- routes/web.go
+++ routes/web.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"github.com/gorilla/mux"
 	"goblog/app/http/controllers"
-	"goblog/app/http/middlewares"
 	"net/http"
 )
 
@@ -26,6 +25,7 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Update).Methods("POST").Name("articles.update")
 	r.HandleFunc("/articles/{id:[0-9]+}/delete", ac.Delete).Methods("POST").Name("articles.delete")
 
-	// 中间件：强制内容类型为 HTML
-	r.Use(middlewares.ForceHTML)
+	// 静态资源
+	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
+	r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './resources/views/**/*.tmpl',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
