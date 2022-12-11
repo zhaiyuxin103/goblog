40501d88284efd899c512a45578ce164e2f3d72b
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-12-11 16:46:34 +0800

    style(articles): 美化文章表单页

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -7,7 +7,6 @@ import (
 	"goblog/pkg/route"
 	"goblog/pkg/view"
 	"gorm.io/gorm"
-	"html/template"
 	"net/http"
 	"strconv"
 	"unicode/utf8"
@@ -20,7 +19,7 @@ type ArticlesController struct {
 // ArticlesFormData 创建博文表单数据
 type ArticlesFormData struct {
 	Title, Body string
-	URL         string
+	Article     article.Article
 	Errors      map[string]string
 }
 
@@ -56,25 +55,13 @@ func (*ArticlesController) Index(w http.ResponseWriter, r *http.Request) {
 		logger.LogError(err)
 	} else {
 		// ---  2. 加载模版 ---
-		view.Render(w, "articles.index", articles)
+		view.Render(w, articles, "articles.index")
 	}
 }
 
 // Create 文章创建页面
 func (*ArticlesController) Create(w http.ResponseWriter, r *http.Request) {
-
-	storeURL := route.Name2URL("articles.store")
-	data := ArticlesFormData{
-		Title:  "",
-		Body:   "",
-		URL:    storeURL,
-		Errors: nil,
-	}
-	tmpl, err := template.ParseFiles("resources/views/articles/create.tmpl")
-	logger.LogError(err)
-
-	err = tmpl.Execute(w, data)
-	logger.LogError(err)
+	view.Render(w, ArticlesFormData{}, "articles.create", "articles._form_field")
 }
 
 // Store 创建文章
@@ -102,19 +89,11 @@ func (*ArticlesController) Store(w http.ResponseWriter, r *http.Request) {
 			logger.LogError(err)
 		}
 	} else {
-		storeURL := route.Name2URL("articles.store")
-
-		data := ArticlesFormData{
+		view.Render(w, ArticlesFormData{
 			Title:  title,
 			Body:   body,
-			URL:    storeURL,
 			Errors: errors,
-		}
-		tmpl, err := template.ParseFiles("resources/views/articles/create.tmpl")
-		logger.LogError(err)
-
-		err = tmpl.Execute(w, data)
-		logger.LogError(err)
+		}, "articles.create", "articles._form_field")
 	}
 }
 
@@ -142,7 +121,7 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 		}
 	} else {
 		// ---  4. 读取成功，显示文章 ---
-		view.Render(w, "articles.show", article)
+		view.Render(w, article, "articles.show")
 	}
 }
 
@@ -153,7 +132,7 @@ func (*ArticlesController) Edit(w http.ResponseWriter, r *http.Request) {
 	id := route.GetRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
-	article, err := article.Get(id)
+	_article, err := article.Get(id)
 
 	// 3. 如果出现错误
 	if err != nil {
@@ -170,19 +149,13 @@ func (*ArticlesController) Edit(w http.ResponseWriter, r *http.Request) {
 			logger.LogError(err)
 		}
 	} else {
-		// 4. 读取成功，显示表单
-		updateURL := route.Name2URL("articles.update", "id", id)
-		data := ArticlesFormData{
-			Title:  article.Title,
-			Body:   article.Body,
-			URL:    updateURL,
-			Errors: nil,
-		}
-		tmpl, err := template.ParseFiles("resources/views/articles/edit.tmpl")
-		logger.LogError(err)
-
-		err = tmpl.Execute(w, data)
-		logger.LogError(err)
+		// 4. 读取成功，显示编辑文章表单页
+		view.Render(w, ArticlesFormData{
+			Title:   _article.Title,
+			Body:    _article.Body,
+			Article: _article,
+			Errors:  nil,
+		}, "articles.edit", "articles._form_field")
 	}
 }
 
@@ -244,19 +217,12 @@ func (*ArticlesController) Update(w http.ResponseWriter, r *http.Request) {
 		} else {
 
 			// 4.3 表单验证不通过，显示理由
-
-			updateURL := route.Name2URL("articles.update", "id", id)
-			data := ArticlesFormData{
-				Title:  title,
-				Body:   body,
-				URL:    updateURL,
-				Errors: errors,
-			}
-			tmpl, err := template.ParseFiles("resources/views/articles/edit.tmpl")
-			logger.LogError(err)
-
-			err = tmpl.Execute(w, data)
-			logger.LogError(err)
+			view.Render(w, ArticlesFormData{
+				Title:   title,
+				Body:    body,
+				Article: _article,
+				Errors:  errors,
+			}, "articles.edit", "articles._form_field")
 		}
 	}
 }
--- pkg/view/view.go
+++ pkg/view/view.go
@@ -10,27 +10,30 @@ import (
 )
 
 // Render 渲染视图
-func Render(w io.Writer, name string, data interface{}) {
+func Render(w io.Writer, data interface{}, tplFiles ...string) {
 	// 1. 设置模版相对路径
 	viewDir := "resources/views/"
 
-	// 2. 语法糖，将 articles.show 更正为 articles/show
-	name = strings.Replace(name, ".", "/", -1)
+	// 2. 遍历传参文件列表 Slice，设置正确的路径，支持 dir.filename 语法糖
+	for i, f := range tplFiles {
+		tplFiles[i] = viewDir + strings.Replace(f, ".", "/", -1) + ".tmpl"
+	}
 
 	// 3. 所有布局模版文件 Slice
-	files, err := filepath.Glob(viewDir + "layouts/*.tmpl")
+	layoutFiles, err := filepath.Glob(viewDir + "layouts/*.tmpl")
 	logger.LogError(err)
 
-	// 4. 在 Slice 里新增目标文件
-	newFiles := append(files, viewDir+name+".tmpl")
+	// 4. 合并所有文件
+	allFiles := append(layoutFiles, tplFiles...)
 
 	// 5. 解析所有模版文件
-	tmpl, err := template.New(name + ".tmpl").
+	tmpl, err := template.New("").
 		Funcs(template.FuncMap{
 			"RouteName2URL": route.Name2URL,
-		}).ParseFiles(newFiles...)
+		}).ParseFiles(allFiles...)
 	logger.LogError(err)
 
 	// 6. 渲染模版
 	err = tmpl.ExecuteTemplate(w, "app", data)
+	logger.LogError(err)
 }
