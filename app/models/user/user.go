be5ecd1806d7a3f37ef524f3540369015476f7e1
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-12-26 14:27:51 +0800

    feat(auth): 会话和认证管理

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -15,3 +15,8 @@ type User struct {
 
 	models.CommonTimestampsField
 }
+
+// ComparePassword 对比密码是否匹配
+func (user *User) ComparePassword(password string) bool {
+	return user.Password == password
+}
