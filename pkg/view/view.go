cd2039c3d4efc9fd39b8fbcd1686113ee491681d
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-12-28 13:02:13 +0800

    feat(articles): 显示作者

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -1,6 +1,7 @@
 package view
 
 import (
+	"goblog/app/models/user"
 	"goblog/pkg/auth"
 	"goblog/pkg/flash"
 	"goblog/pkg/logger"
@@ -31,6 +32,7 @@ func RenderTemplate(w io.Writer, name string, data D, tplFiles ...string) {
 	data["isLogined"] = auth.Check()
 	data["loginUser"] = auth.User
 	data["flash"] = flash.All()
+	data["Users"], _ = user.All()
 
 	// 2. 生成模版文件
 	allFiles := getTemplateFiles(tplFiles...)
